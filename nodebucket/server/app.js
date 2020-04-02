fd225c0c630b7e1aef848cd96f9eca2f0107c947
Gabriel Sanchez <gjsanchez@my365.bellevue.edu>
2020-04-01 19:15:12 -0500

    update

--- nodebucket/server/app.js
+++ nodebucket/server/app.js
@@ -34,7 +34,7 @@ app.use('/', express.static(path.join(__dirname, '../dist/nodebucket')));
 /**
  * Variables
  */
-const port = 3000; // server port
+const port = process.env.PORT || "3000"; // server port
 
 // TODO: This line will need to be replaced with your actual database connection string
 const conn = "mongodb+srv://Gabriel:Jairo500!@cluster0-djivq.gcp.mongodb.net/nodebucket?retryWrites=true";
